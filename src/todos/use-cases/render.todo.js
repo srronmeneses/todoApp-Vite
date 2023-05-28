132f7603e4c250d16f253f7df7e50302d1342d79
sr.ronmeneses <sr.ronmeneses@gmail.com>
2023-05-27 22:58:20 -0400

    Mostrando todoStore en el DOM

--- src/todos/use-cases/render.todo.js
+++ src/todos/use-cases/render.todo.js
@@ -1,13 +1,15 @@
 import { Todo } from "../models/todo.model";
 import { todoHtml } from "./todo.html";
-
+let element;
 /**
  * 
  * @param {String} elementId 
  * @param {Todo} todo 
  */
 export const renderTodo = (elementId, todo = []) => {
-    const element = document.querySelector(elementId);
+    if(!element) element = document.querySelector(elementId);
+    if(!element) throw new Error(`element ${elementId} not found`);
+    element.innerHTML = ``;
     todo.forEach(todo => {
         element.append(todoHtml(todo))
     });
--- src/todos/use-cases/todo.html.js
+++ src/todos/use-cases/todo.html.js
@@ -6,7 +6,16 @@ import { Todo } from "../models/todo.model";
  */
 export const todoHtml = (todo) => {
     const liElement = document.createElement(`li`);
-    const html = `<h1>${todo.description}</h1>`;
+    const {id, description, done} = todo;
+    const html = `
+    <div class="view">
+        <input class="toggle" type="checkbox" ${done ? `checked`: ``}>
+        <label>${description}</label>
+        <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">`;
+    liElement.setAttribute(`data-id`, id);
+    if(done) liElement.classList.add(`completed`);
     liElement.innerHTML = html;
     return liElement;
 }
